ed4fd5fbb66b34b092488a7551912b1173988d0c
Nayan Makvana <nayan.xt@outlook.com>
2023-11-18 00:13:08 +0530

    new changes

---
+++ EventManagemenet/Controllers/PrintOrderController.cs
@@ -0,0 +1,36 @@
+﻿using EventManagemenet.Filters;
+using EventManagemenetApp.DataAccess.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagemenet.Controllers
+{
+    [ValidateUserSession]
+    public class PrintOrderController : Controller
+    {
+        private ITotalbilling _ITotalbilling;
+        public PrintOrderController(ITotalbilling ITotalbilling)
+        {
+            _ITotalbilling = ITotalbilling;
+        }
+
+        [HttpGet]
+        public IActionResult Print(string BookingNo)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(BookingNo))
+                {
+                    return RedirectToAction("AllBookings", "ShowBookingDetails");
+                }
+
+                var details = _ITotalbilling.GetBillingDetailsbyBookingNo(BookingNo);
+
+                return View(details);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
